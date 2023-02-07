a08701cb951ed7bd5f651873b10f901b9b0ba61b
azimovgleb <azimov.gleb96@gmail.com>
2023-02-07 23:47:06 +0300

    Init commit

---
+++ src/test/java/com/gleb/demo/SpringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
